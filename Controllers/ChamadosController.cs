e7dba6415ac9c4bdf58bb6d6330f3aa537db6849
Isac Fernandes <isacfernandes35@gmail.com>
2026-08-14 07:39:50 -0300

    Cria estrutura inicial da Web Api

---
+++ Controllers/ChamadosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaChamados.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ChamadosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("API de chamados funcionando!");
+        }
+    }
+}
